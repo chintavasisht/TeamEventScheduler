0b400330643ced75517808471e343f84a30faed4
Vasisht Chinta <vasisht.chinta@assurant.com>
2023-07-30 10:00:35 -0400

    Initial Commit

---
+++ samplelogin/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  samplelogin
+//
+//  Created by Vasisht Chinta on 7/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ samplelogin/sampleloginApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleloginApp.swift
+//  samplelogin
+//
+//  Created by Vasisht Chinta on 7/30/23.
+//
+
+import SwiftUI
+
+@main
+struct sampleloginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
